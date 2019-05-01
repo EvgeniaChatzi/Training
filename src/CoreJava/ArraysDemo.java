0ffeba0f07eb2f540a2e5bc0cbe1769bf8e6c3d6
Evgenia Chatzi <Evgenia.Chatzi@CE00939-mac.local>
2019-05-01 16:56:30 +0200

    Interfaces

---
+++ src/CoreJava/ArraysDemo.java
@@ -0,0 +1,17 @@
+package CoreJava;
+
+public class ArraysDemo {
+
+    public static void main(String[] args) {
+
+
+        int[] a = {2,7,9,5,90};
+
+        System.out.println(a[0]);
+
+        for (int i =0; i<a.length; i++) {
+
+            System.out.println(a[i]);
+        }
+    }
+}
---
+++ src/InterfaceConcept/BankingClient.java
@@ -0,0 +1,11 @@
+package InterfaceConcept;
+
+public interface BankingClient {
+
+    public void paycreditcard();
+    public void trancferbalance();
+    public void checkingbalance();
+
+
+
+}
